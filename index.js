498999dae5befaf5f0ee5adaa8b1dbd5e267ba94
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-03-25 23:16:07 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    // Use Array.prototype.filter() to filter out drivers with exact match
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    // Use Array.prototype.filter() to filter out drivers with beginning matching letters
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  function matchName(drivers, name) {
+    
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
